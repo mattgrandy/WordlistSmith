ea6cd7d81da9d8473a8b75c4ff90d1a1910b9e88
Matt Grandy <mgrandy@fortynorthsecurity.com>
2021-03-10 15:09:18 -0700

    First commit

---
+++ WordlistSmith/Program.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Abot2.Core;
+using Abot2.Crawler;
+using Abot2.Poco;
+using CommandLine;
+using CommandLine.Text;
+using NUglify;
+using Serilog;
+
+namespace WordlistSmith
+{
+	class Program
+	{
+		private static HashSet<string> wordlist = new HashSet<string>();
+		public static Smither smither;
+
+		static async Task Main(string[] args)
+		{
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            // Parse arguments passed
+            Parser parser = new Parser(with =>
+            {
+                with.CaseInsensitiveEnumValues = true;
+                with.CaseSensitive = false;
+                with.HelpWriter = null;
+            });
+
+            ParserResult<Options> parserResult = parser.ParseArguments<Options>(args);
+            parserResult.WithParsed<Options>(o => { Options.Instance = o; })
+                .WithNotParsed(errs => DisplayHelp(parserResult, errs));
+            Options options = Options.Instance;
+
+            try
+            {
+                smither = new Smither();
+
+				if (options.Verbose)
+				{
+					Log.Logger = new LoggerConfiguration()
+						.MinimumLevel.Information()
+						.WriteTo.Console()
+						.CreateLogger();
+				}
+
+				else
+				{
+					Log.Logger = new LoggerConfiguration()
+						.MinimumLevel.Error()
+						.WriteTo.Console()
+						.CreateLogger();
+				}
+
+				await DoCrawl(smither);
+
+				Console.WriteLine("\nCrawl finished, writing to file...\n");
+
+				if (string.IsNullOrEmpty(smither.Output))
+					smither.Output = "wordlist_" + DateTime.Now.ToString("M-dd-yyyy_HH-mm-ss") + ".txt";
+
+				using (StreamWriter outfile = new StreamWriter(smither.Output))
+				{
+					foreach (var word in wordlist)
+						await outfile.WriteLineAsync(word);
+				}
+
+				watch.Stop();
+				Console.WriteLine("Execution time: " + watch.ElapsedMilliseconds / 1000 + " Seconds");
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+		}
+
+        private static async Task DoCrawl(Smither smither)
+        {
+	        var config = new CrawlConfiguration();
+	        if (!string.IsNullOrEmpty(smither.User) && !string.IsNullOrEmpty(smither.Pass))
+	        {
+		        config.MaxConcurrentThreads = smither.Threads;
+		        config.MaxCrawlDepth = smither.Depth;
+		        config.MinCrawlDelayPerDomainMilliSeconds = smither.Delay;
+		        config.MaxPagesToCrawl = smither.MaxPages;
+		        config.MaxRetryCount = 1;
+		        //HttpServicePointConnectionLimit = 2000,
+		        config.HttpRequestTimeoutInSeconds = smither.Timeout;
+		        config.LoginUser = smither.User;
+		        config.LoginPassword = smither.Pass;
+	        }
+
+	        if (!string.IsNullOrEmpty(smither.User) || !string.IsNullOrEmpty(smither.Pass))
+	        {
+		        if (string.IsNullOrEmpty(smither.Pass) || string.IsNullOrEmpty(smither.User))
+		        {
+			        Console.WriteLine("Please specify both a username and a password if using basic auth");
+			        System.Environment.Exit(1);
+		        }
+	        }
+
+			else
+	        {
+		        config.MaxConcurrentThreads = smither.Threads;
+		        config.MaxCrawlDepth = smither.Depth;
+		        config.MinCrawlDelayPerDomainMilliSeconds = smither.Delay;
+		        config.MaxPagesToCrawl = smither.MaxPages;
+		        config.MaxRetryCount = 1;
+		        //HttpServicePointConnectionLimit = 2000,
+		        config.HttpRequestTimeoutInSeconds = smither.Timeout;
+	        }
+
+	        var crawler = new PoliteWebCrawler(config);
+
+			crawler.PageCrawlCompleted += PageCrawlCompleted;
+
+	        var crawlResult = await crawler.CrawlAsync(new Uri(smither.Url));
+        }
+
+        private static async Task DemoSinglePageRequest()
+        {
+	        var pageRequester = new PageRequester(new CrawlConfiguration(), new WebContentExtractor());
+
+	        var crawledPage = await pageRequester.MakeRequestAsync(new Uri("http://msn.com"));
+	        Log.Logger.Information("{result}", new
+	        {
+		        url = crawledPage.Uri,
+		        status = Convert.ToInt32(crawledPage.HttpResponseMessage.StatusCode)
+	        });
+        }
+
+		private static void PageCrawlCompleted(object sender, PageCrawlCompletedArgs e)
+        {
+	        var httpStatus = e.CrawledPage.HttpResponseMessage?.StatusCode;
+	        var rawPageText = e.CrawledPage.Content.Text;
+	        char[] charsToRemove = {' ', ';', '.', '!', ':', '=', '@', '#', '$', '"', '%', '^', '&', '*', '(', ')', '<', '>', '?', '\'', ',', '-'};
+
+			if (httpStatus == HttpStatusCode.OK)
+	        {
+		        //Log.Logger.Information("{result}", new
+		        //{
+			       // url = e.CrawledPage.Uri,
+			       // status = Convert.ToInt32(e.CrawledPage.HttpResponseMessage.StatusCode)
+		        //});
+
+				//lock (rawPageText)
+				//{
+					List<string> tempList = new List<string>();
+
+					var result = Uglify.HtmlToText(rawPageText).ToString();
+					foreach (var word in result.Split())
+					{
+						string cleanWord = word.ToLower().Trim(charsToRemove);
+
+						if (cleanWord.Length > smither.Min && cleanWord.Length < 256)
+						{
+							if (!wordlist.Contains(cleanWord))
+								tempList.Add(cleanWord);
+						}
+					}
+
+					foreach (var word in tempList)
+						wordlist.Add(word);
+				//}
+			}
+        }
+
+		public class Options
+        {
+            public static Options Instance { get; set; }
+
+            // Command line options
+            [Option('v', "verbose", Required = false, HelpText = "Set output to verbose")]
+            public bool Verbose { get; set; }
+
+            [Option('u', "url", Required = true, HelpText = "Specify a URL to scrape words from")]
+            public string Url { get; set; }
+
+            [Option('o', "output", Required = false, HelpText = "Specify a file to output the wordlist to", Default = null)]
+            public string Output { get; set; }
+
+            [Option("depth", Required = false, HelpText = "Specify the depth to crawl", Default = 3)]
+            public int Depth { get; set; }
+
+            [Option("max-pages", Required = false, HelpText = "Specify the maximum pages to crawl", Default = null)]
+            public int MaxPages { get; set; }
+
+			[Option('t', "threads", Required = false, HelpText = "Specify the number of concurrent threads", Default = 10)]
+            public int Threads { get; set; }
+
+			[Option("min-length", Required = false, HelpText = "Specify a minimum word length to save", Default = 3)]
+            public int Minimum { get; set; }
+
+            [Option("max-length", Required = false, HelpText = "Specify a maximum word length to save", Default = null)]
+            public int Maximum { get; set; }
+
+            [Option("delay", Required = false, HelpText = "Specify a delay between requests", Default = 100)]
+            public int Delay { get; set; }
+
+            [Option("timeout", Required = false, HelpText = "Specify a timeout for each request", Default = 15)]
+            public int Timeout { get; set; }
+
+            [Option("user", Required = false, HelpText = "Specify a username for basic auth")]
+            public string User { get; set; }
+
+            [Option("pass", Required = false, HelpText = "Specify a password for basic auth")]
+            public string Pass { get; set; }
+
+			[Option('a', "agent", Required = false, HelpText = "Specify a User Agent to use", 
+	            Default = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36")]
+            public string Agent { get; set; }
+		}
+
+        private static void DisplayHelp<T>(ParserResult<T> result, IEnumerable<Error> errs)
+        {
+            HelpText helpText = HelpText.AutoBuild(result, h =>
+            {
+                h.AdditionalNewLineAfterOption = false;
+                h.Heading = "WordSmith C# Version 0.1"; //change header
+                h.Copyright = ""; //change copyright text
+                h.AutoVersion = false;
+                return HelpText.DefaultParsingErrorsHandler(result, h);
+            }, e => e);
+            Console.WriteLine(helpText);
+            System.Environment.Exit(1);
+        }
+    }
+}
---
+++ WordlistSmith/Smither.cs
@@ -0,0 +1,25 @@
+﻿
+namespace WordlistSmith
+{
+    public class Smither
+    {
+        public string Url, Output, Agent, User, Pass;
+        public int Min, Max, Depth, Delay, Threads, MaxPages, Timeout;
+
+        public Smither()
+        {
+            this.Url = Program.Options.Instance.Url;
+            this.Min = Program.Options.Instance.Minimum;
+            this.Max = Program.Options.Instance.Maximum;
+            this.Output = Program.Options.Instance.Output;
+            this.Depth = Program.Options.Instance.Depth;
+            this.Threads = Program.Options.Instance.Threads;
+            this.Delay = Program.Options.Instance.Delay;
+            this.Agent = Program.Options.Instance.Agent;
+            this.MaxPages = Program.Options.Instance.MaxPages;
+            this.Timeout = Program.Options.Instance.Timeout;
+            this.User = Program.Options.Instance.User;
+            this.Pass = Program.Options.Instance.Pass;
+        }
+    }
+}
